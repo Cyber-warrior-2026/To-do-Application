6d47f0da8ed66a58a5c5691574f188a926154ede
Arjun Singh Bhadauriya <arjun22august@gmail.com>
2026-01-20 16:44:18 +0530

    Updated the Routes

--- backend/src/modules/auth/auth.routes.ts
+++ backend/src/modules/auth/auth.routes.ts
@@ -3,10 +3,14 @@ import { AuthController } from './auth.controller';
 
 const router = Router();
 
-
+//OAuth Login
 router.post('/google', AuthController.googleLogin);
 
+//Register and Login without OAuth
+router.post('/register', AuthController.register);
+router.post('/login', AuthController.login);
 
+//Logout
 router.post('/logout', AuthController.logout);
 
 export default router;
