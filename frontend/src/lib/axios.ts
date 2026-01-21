b46c902814ee6ab71e78f5998c5ae791bd0d9fd3
Arjun Singh Bhadauriya <arjun22august@gmail.com>
2026-01-22 00:03:06 +0530

    Revert the channges

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+
 const api = axios.create({
-  baseURL: BASE_URL, 
+  baseURL: 'http://localhost:4000/api', 
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
